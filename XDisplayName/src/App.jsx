d6d9c13d04097009fa9dd4ac0ce9f41dffbc1b20
Sujwalnaik <sujwalbn.a0@gmail.com>
2024-07-06 15:11:30 +0530

    resolved failing test cases

--- XDisplayName/src/App.jsx
+++ XDisplayName/src/App.jsx
@@ -1,46 +1,3 @@
-// import { useState } from "react";
-
-// function App() {
-//   const [fullname, setFullName] = useState("");
-//   const [firstName, setFirstName] = useState("");
-//   const [lastName, setLastName] = useState("");
-
-//   const handleFullname = (e) => {
-//     e.preventDefault();
-//     setFullName(`${firstName} ${lastName}`);
-//     setFirstName("");
-//     setLastName("");
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={handleFullname}>
-//         <h1>Full Name Display</h1>
-//         <div>
-//           <label htmlFor="fname">First Name : </label>
-//           <input
-//             type="text"
-//             id="fname"
-//             onChange={(e) => setFirstName(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="lname">Last Name : </label>
-//           <input
-//             type="text"
-//             id="lname"
-//             onChange={(e) => setLastName(e.target.value)}
-//             required
-//           />
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//       <div>Full Name: {fullname}</div>
-//     </div>
-//   );
-// }
-
-// export default App;
 import { useState } from "react";
 
 function App() {
@@ -94,7 +51,7 @@ function App() {
           Submit
         </button>
       </form>
-      {fullName && <div>Full Name :{fullName}</div>}
+      {fullName && <div>Full Name: {fullName}</div>}
     </div>
   );
 }
